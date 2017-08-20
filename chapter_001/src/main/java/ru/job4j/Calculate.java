925d065d63173c50ba1f5323178070632cc23697
pacificfwy <pacificfwy@gmail.com>
2017-08-20 00:59:40 -0400

    Checkstyle tool added

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,19 +1,19 @@
 package ru.job4j;
 
 /**
-* Класс для вычисления арифметических операций + - * / ^
+* Класс для вычисления арифметических операций + - * / ^.
 * @author aponomarev
 * @since Aug 19, 2017
 * @version 1
 */
 
 public class Calculate {
-	
+
 	/**
-	* Вывод строки в консоль
+	* Вывод строки в консоль.
 	* @param args - args
 	*/
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
