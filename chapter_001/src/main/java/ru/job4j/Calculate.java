a9c01e89f970361a61c533d680a073062c24d6e9
pacificfwy <pacificfwy@gmail.com>
2017-08-19 22:25:06 -0400

    JavaDoc, package-info added #4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,20 @@
 package ru.job4j;
 
-public class Calculate
-{
-	public static void main(String[] args)
-	{
+/**
+* Класс для вычисления арифметических операций + - * / ^
+* @author aponomarev
+* @since Aug 19, 2017
+* @version 1
+*/
+
+public class Calculate {
+	
+	/**
+	* Вывод строки в консоль
+	* @param args - args
+	*/
+	
+	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
 }
